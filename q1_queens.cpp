f065295ac2d1a7fa56b8895cd22cd444c226a059
onebelowall1218 <anjumulazim@gmail.com>
2025-06-02 13:17:27 +0530

    first commit

---
+++ q1_queens.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+
+private:
+    void backtrack(int row, int n, vector<string> &board, vector<bool> &cols, vector<bool> &diag1, vector<bool> &diag2, vector<vector<string>> &result){
+        if(row==n){
+            result.push_back(board);
+            return;
+        }
+
+        for(int col=0;col<n;col++){
+            if(cols[col] || diag1[row-col+n-1] || diag2[row+col]){
+                continue;
+            }
+
+            board[row][col]='Q';
+            cols[col]=true;
+            diag1[row-col+n-1]=true;
+            diag2[row+col]=true;
+            
+            backtrack(row+1, n, board, cols, diag1, diag2, result);
+            
+            board[row][col]='.';
+            cols[col]=false;
+            diag1[row-col+n-1]=false;
+            diag2[row+col]=false;
+            
+        }
+    }
+
+public:
+    vector<vector<string>> nQueens(int n){
+        vector<vector<string>> result;
+        vector<string> board(n, string(n, '.'));
+        vector<bool> cols(n, false);
+        vector<bool> diag1(2*n-1, false);
+        vector<bool> diag2(2*n-1, false);
+        backtrack(0, n, board, cols, diag1, diag2, result);
+        return result;
+    }
+
+    void displayBoards(vector<vector<string>> &boards){
+        for(auto &board: boards){
+            for(auto row: board){
+                cout<<row<<endl;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    system("cls");
+    Solution s;
+    int n;
+    cin>>n;
+    vector<vector<string>> result = s.nQueens(n);
+    s.displayBoards(result);
+    return 0;
+}
---
+++ q2_hasCircularDependency.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+private:
+    bool dfs(int currNode, vector<vector<int>> &adj, vector<bool> &visited, vector<bool> &recurStack){
+        visited[currNode]=true;
+        recurStack[currNode]=true;
+
+        for(int &node: adj[currNode]){
+            if(!visited[node] && dfs(node, adj, visited, recurStack)){
+                return true;
+            }
+            if(recurStack[node]){
+                return true;
+            }
+        }
+        recurStack[currNode]=false;
+
+        return false;
+    }
+public:
+    bool hasCircularDependency(int n, vector<vector<int>> &edges){
+        vector<vector<int>> adj(n);
+        for(auto &e:edges){
+            adj[e[0]].push_back(e[1]);
+        }
+        vector<bool> visited(n, false);
+        vector<bool> recurStack(n, false);
+        
+        for(int node=0;node<n;node++){
+            if(!visited[node] && dfs(node, adj, visited, recurStack)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main(){
+    system("cls");
+    Solution s;
+    int n = 4;
+    vector<vector<int>> edges = {{0, 1}, {1, 2}, {2, 3}};
+    bool result = s.hasCircularDependency(n, edges);
+    cout<<result<<endl;
+
+    n = 4;
+    edges = {{0, 1}, {1, 2}, {2, 0}};
+    result = s.hasCircularDependency(n, edges);
+    cout<<result<<endl;
+    return 0;
+}
